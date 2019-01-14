39a68c2ee9ad26b155d5e994ae42b580c7e87d48
kazukitokunaga <tokunagakazuki330@gmail.com>
2019-01-14 19:55:24 +0900

    middlewareによるログイン機能の実装

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,20 @@
-export default () => {
-  if (process.browser) {
-    console.log('console.log() on browser');
-  } else {
-    console.log('console.log() on SSR server');
+import Cookies from 'universal-cookie'
+
+export default ({ req, route, redirect }) => {
+  console.log(route.path);
+  // '/'または'/home'を参照された場合は認証をスキップ
+  if (['/'].includes(route.path) || ['/home'].includes(route.path)) {
+    return
+  }
+  const cookies = req ? new Cookies(req.headers.cookies) : new Cookies()
+  const credential = cookies.get('credential')
+
+  // もしcredentialが存在して、かつ、loginページにアクセスしている場合はトップページに飛ばす
+  if (credential && route.path === '/login') {
+    return redirect('/home')
+  }
+  // もしcredentialが存在せず、かつ、ログインページにアクセスしている場合はログインページに飛ばす
+  if (!credential && route.path !== '/login') {
+    return redirect('/login')
   }
 }
