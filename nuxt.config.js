c60975e406f9740a3d3ac42072ba263fd4c64b9d
kazukitokunaga <tokunagakazuki330@gmail.com>
2019-01-14 18:56:13 +0900

    認証情報を付与したAPIとdirenvによる設定（export QIITA_TOKEN=<取得したトークン>を.envrcファイルに記載）

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,12 @@ module.exports = {
   axios: {
 
   },
+  plugins: [
+    '~/plugins/axios.js'
+  ],
+  env: {
+    QIITA_TOKEN: process.env.QIITA_TOKEN
+  },
   /*
   ** Build configuration
   */
---
+++ plugins/axios.js
@@ -0,0 +1,10 @@
+import { config } from "rx";
+
+export default function({ $axios }) {
+  $axios.onRequest((config)) => {
+    if (process.env.QIITA_TOKEN) {
+      config.headers.common['Authorization'] = process.env.QIITA_TOKEN
+    }
+    return config
+  }
+}
